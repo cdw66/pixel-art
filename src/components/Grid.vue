7717b34dad57a6b0611454dca2022fb5e0f602e9
Carter Wein <cdw99@cornell.edu>
2021-11-27 17:56:10 -0500

    Fixed highlighting issue

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :key="pixel.id" v-for="pixel in pixels">
+  <div :key="pixel.id" v-for="pixel in pixels" class="unselectable">
     <!-- <Pixel @color-pixel="$emit('color-pixel', pixel.id)" :pixel="pixel" /> -->
     <Pixel
       @start-color="$emit('start-color')"
@@ -26,4 +26,11 @@ export default {
 </script>
 
 <style scoped>
+.unselectable {
+  -webkit-user-drag: none;
+  user-select: none;
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+}
 </style>
